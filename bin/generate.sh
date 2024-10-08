5860a55da40f17f06a5071487f036119bb367b04
Giovanni Grano <me@giograno.com>
2024-10-08 11:04:06 -0400

    fix get requests with a body

--- bin/generate.sh
+++ bin/generate.sh
@@ -7,5 +7,6 @@ docker run --rm -v "${PWD}:/local" openapitools/openapi-generator-cli generate \
     -o /local/localstack-sdk-generated \
     --global-property models,apis,supportingFiles \
     -p packageName=localstack \
+    --template-dir /local/localstack-sdk-generated/templates \
     --global-property apiTests=false,modelTests=false \
     --global-property apiDocs=false,modelDocs=False
--- localstack-sdk-python/localstack/clients.py
+++ localstack-sdk-python/localstack/clients.py
@@ -1,3 +1,5 @@
+import os
+
 from localstack.api_client import ApiClient
 from localstack.configuration import Configuration
 
@@ -6,8 +8,10 @@ class BaseClient:
     """A BaseClient creates a configuration and instantiate a ApiClient"""
     configuration: Configuration
     _api_client: ApiClient
+    auth_token: str | None
 
     def __init__(self, host: str | None = None, auth_token: str | None = None, **kwargs) -> None:
         _host = host or "http://localhost.localstack.cloud:4566"
-        self.configuration = Configuration(host=_host, access_token=auth_token)
+        self.auth_token = auth_token or os.getenv("LOCALSTACK_AUTH_TOKEN", "").strip("'\" ")
+        self.configuration = Configuration(host=_host)
         self._api_client = ApiClient(configuration=self.configuration)
